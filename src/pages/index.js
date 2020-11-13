1be7225b8458ed86e63773df487a5eea11a8a604
david <davidluengo8@gmail.com>
2020-11-13 19:32:54 +0100

    navbar and footer (not following convention)

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,13 +1,17 @@
 import { Link } from "gatsby"
 import React from "react"
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
 
 export default function Home() {
   return (
     <div>
+      <Navbar />
       Hello world!
       <div>
         <Link to="/blog/">blog page</Link>
       </div>
+      <Footer />
     </div>
   )
 }
