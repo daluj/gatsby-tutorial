16b0de47baf3a5e015d7b125c3ba87137c6b868c
david <davidluengo8@gmail.com>
2020-11-16 09:06:46 +0100

    css modules

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,15 +1,18 @@
 import React from "react"
 import Layout from "../components/Layout"
+import styles from "../components/blog.module.css"
 
 const blog = () => {
   return (
     <Layout>
-      <h1> This is our blog</h1>
-      <p className="blog-text">
-        Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum Lorem
-        ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem
-        ipsum
-      </p>
+      <div className={styles.page}>
+        <h1 className={styles.page.h1}> This is our blog</h1>
+        <p className={styles.text}>
+          Lorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsumLorem ipsum
+          Lorem ipsum Lorem ipsum Lorem ipsumLorem ipsumLorem ipsumLorem
+          ipsumLorem ipsum
+        </p>
+      </div>
     </Layout>
   )
 }
--- src/pages/products.js
+++ src/pages/products.js
@@ -1,10 +1,16 @@
 import React from "react"
 import Layout from "../components/Layout"
+import styles from "../components/products.module.css"
 
 const products = () => {
   return (
     <Layout>
-      <h1>This is our products page</h1>
+      <div className={styles.page}>
+        <h1 className={styles.page.h1}>This is our products page</h1>
+        <p className={styles.text}>
+          Lorem ipsum dorlorasd hic, asdfkjwerit jfguiowe r
+        </p>
+      </div>
     </Layout>
   )
 }
