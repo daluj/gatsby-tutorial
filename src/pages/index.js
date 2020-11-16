bb8560103ad4ae11084051a352ca0474b198b329
david <davidluengo8@gmail.com>
2020-11-16 09:24:20 +0100

    creating styled components

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from "react"
 import Layout from "../components/Layout"
+import { ExampleButton } from "../components/button"
 
 export default function Home() {
   return (
@@ -7,6 +8,7 @@ export default function Home() {
       <h1 style={{ color: "red", textTransform: "uppercase" }}>
         Hello from the other side
       </h1>
+      <ExampleButton>click me</ExampleButton>
     </Layout>
   )
 }
