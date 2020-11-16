18a8b31b26b483c94efcc9c7161decb608dc5df8
david <davidluengo8@gmail.com>
2020-11-16 12:51:49 +0100

    render gatsby images with plugin

--- src/examples/Images.js
+++ src/examples/Images.js
@@ -5,17 +5,17 @@ import Image from "gatsby-image"
 
 const getImages = graphql`
   {
-    fixed: file(relativePath: { eq: "image1.jpeg" }) {
+    fixed: file(relativePath: { eq: "image2.jpeg" }) {
       childImageSharp {
         fixed(width: 300, height: 400) {
-          src
+          ...GatsbyImageSharpFixed
         }
       }
     }
-    fluid: file(relativePath: { eq: "image2.jpeg" }) {
+    fluid: file(relativePath: { eq: "image3.jpeg" }) {
       childImageSharp {
         fluid {
-          src
+          ...GatsbyImageSharpFluid_tracedSVG
         }
       }
     }
@@ -23,16 +23,23 @@ const getImages = graphql`
 `
 
 function Images() {
+  const data = useStaticQuery(getImages)
   return (
     <section className="images">
       <article className="single-image">
         <h3>basic image</h3>
+        <img src={img} width="100%"></img>
       </article>
       <article className="single-image">
         <h3>fixed image/blur</h3>
+        <Image fixed={data.fixed.childImageSharp.fixed} />
       </article>
       <article className="single-image">
         <h3>fluid image/svg</h3>
+        <Image fluid={data.fluid.childImageSharp.fluid} />
+        <div className="small">
+          <Image fluid={data.fluid.childImageSharp.fluid} />
+        </div>
       </article>
     </section>
   )
