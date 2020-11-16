d908c8db7808e8bf2f092df2b6353e6ec1c4c8c5
david <davidluengo8@gmail.com>
2020-11-16 09:46:46 +0100

    site metadata

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,6 +6,13 @@
 
 module.exports = {
   /* Your site config here */
+  siteMetadata: {
+    title: "Gatsby Tutorial",
+    description: "Some baby baby",
+    author: "@johnny",
+    data: ["item 1", "item 2"],
+    person: { name: "peter", age: 45 },
+  },
   plugins: [
     {
       resolve: `gatsby-plugin-styled-components`,
