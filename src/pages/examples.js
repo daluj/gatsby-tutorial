e2373ad825d158153bbe580b498f39eba8eff341
david <davidluengo8@gmail.com>
2020-11-16 10:41:12 +0100

    pagequery graphql

--- src/pages/examples.js
+++ src/pages/examples.js
@@ -1,14 +1,36 @@
 import React from "react"
 import Header from "../examples/Header"
 import Layout from "../components/Layout"
+import { graphql } from "gatsby"
 
-const examples = () => {
+const examples = ({ data }) => {
+  const {
+    site: {
+      info: { description },
+    },
+  } = data
   return (
     <Layout>
       <h1> Hello from examples page</h1>
       <Header />
+      <h3>Description: {description}</h3>
     </Layout>
   )
 }
 
+export const data = graphql`
+  {
+    site {
+      info: siteMetadata {
+        title
+        description
+        author
+        person {
+          name
+          age
+        }
+      }
+    }
+  }
+`
 export default examples
